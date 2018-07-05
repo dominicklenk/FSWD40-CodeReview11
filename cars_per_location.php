e499be18d254c93e98c8a944966dc5762282b68c
dominicklenk <dominic_klenk@yahoo.at>
2018-07-05 20:32:35 +0200

    improved things

--- cars_per_location.php
+++ cars_per_location.php
@@ -2,18 +2,14 @@
 ob_start();
 session_start();
 require_once 'dbconnect.php';
-
     // if session is not set this will redirect to login page
 if( !isset($_SESSION['user']) ) {
         header("Location: index.php");
         exit;
 }
-
     // select logged-in user detail
 $res=mysqli_query($conn, "SELECT * FROM user WHERE user_id=".$_SESSION['user']);
 $userRow=mysqli_fetch_array($res, MYSQLI_ASSOC);
-
-
 ?>
 
 <!--            --------           start HTML                   -------           -->
